d1c530c70ffb5f344350d992d7d6d68d38f48744
June Bowman <juneb67@gmail.com>
2015-06-14 10:45:10 -0400

    Added and changed image files

---
+++ js/app.js
@@ -0,0 +1,53 @@
+//When mouse hovers, fade picture
+$( "#pic1" ).hover(function() {
+  $( "#pic1" ).fadeTo( "fast", 0.4 )
+});
+//When the mouse leaves, remove fade
+$( "#pic1" ).mouseleave(function() {
+  $( "#pic1" ).fadeTo( "fast", 1.0 )
+});
+
+//When mouse hovers, fade picture
+$( "#pic2" ).hover(function() {
+  $( "#pic2" ).fadeTo( "fast", 0.4 )
+});
+//When the mouse leaves, remove fade
+$( "#pic2" ).mouseleave(function() {
+  $( "#pic2" ).fadeTo( "fast", 1.0 )
+});
+
+//When mouse hovers, fade picture
+$( "#pic3" ).hover(function() {
+  $( "#pic3" ).fadeTo( "fast", 0.4 )
+});
+//When the mouse leaves, remove fade
+$( "#pic3" ).mouseleave(function() {
+  $( "#pic3" ).fadeTo( "fast", 1.0 )
+});
+
+//When mouse hovers, fade picture
+$( "#pic4" ).hover(function() {
+  $( "#pic4" ).fadeTo( "fast", 0.4 )
+});
+//When the mouse leaves, remove fade
+$( "#pic4" ).mouseleave(function() {
+  $( "#pic4" ).fadeTo( "fast", 1.0 )
+});
+
+//When mouse hovers, fade picture
+$( "#pic5" ).hover(function() {
+  $( "#pic5" ).fadeTo( "fast", 0.4 )
+});
+//When the mouse leaves, remove fade
+$( "#pic5" ).mouseleave(function() {
+  $( "#pic5" ).fadeTo( "fast", 1.0 )
+});
+
+//When mouse hovers, fade picture
+$( "#pic6" ).hover(function() {
+  $( "#pic6" ).fadeTo( "fast", 0.4 )
+});
+//When the mouse leaves, remove fade
+$( "#pic6" ).mouseleave(function() {
+  $( "#pic6" ).fadeTo( "fast", 1.0 )
+});
